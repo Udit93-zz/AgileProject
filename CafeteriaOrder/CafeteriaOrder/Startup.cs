97e766b9e3ac3a12828302f9e8cdfae450e18b2d
udbhutan <udbhutan@microsoft.com>
2016-08-25 10:37:09 +0530

    Initial Project

---
+++ CafeteriaOrder/CafeteriaOrder/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CafeteriaOrder.Startup))]
+namespace CafeteriaOrder
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
